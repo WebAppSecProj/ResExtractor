2e5c07712512ea4a19f2adc53f46b6a182675b76
Fy <756292457@qq.com>
2020-12-15 19:10:39 +0800

    fix bugs in BufanApp module

--- libs/modules/BufanApp/BufanApp.py
+++ libs/modules/BufanApp/BufanApp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-
+import json
 import logging
 import shutil
 import sys
@@ -41,24 +41,32 @@ class BufanApp(BaseModule):
         os.makedirs(tmp_folder, exist_ok=True)
 
         zf = zipfile.ZipFile(self.detect_file, 'r')
-        config_file = zf.extract("assets/source/config.json", tmp_folder)
-        decode_jar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "decode_bufan.jar")
-        MyJSON_jar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "MyJSON.jar")
-        jvmPath = jpype.getDefaultJVMPath()
-        if not jpype.isJVMStarted():
-            jpype.startJVM(jvmPath, '-ea', '-Djava.class.path={0}:{1}'.format(decode_jar_path, MyJSON_jar_path),
-                           convertStrings=False)
-        jclass = jpype.JClass("com.decode.Main")()
-        appUrl = str(jclass.get_appUrl(config_file))  # cast to str
+        appUrl = ""
+        if "assets/source/dconfig.json" in zf.namelist():
+            config_file = zf.extract("assets/source/dconfig.json", tmp_folder)
+            fo = open(config_file, "r+")
+            config_json = json.load(fo)
+            appUrl = config_json["url"]
+            log.info(config_json)
+        else:
+            config_file = zf.extract("assets/source/config.json", tmp_folder)
+            decode_jar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "decode_bufan.jar")
+            MyJSON_jar_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "MyJSON.jar")
+            jvmPath = jpype.getDefaultJVMPath()
+            if not jpype.isJVMStarted():
+                jpype.startJVM(jvmPath, '-ea', '-Djava.class.path={0}:{1}'.format(decode_jar_path, MyJSON_jar_path),
+                               convertStrings=False)
+            jclass = jpype.JClass("com.decode.Main")()
+            appUrl = str(jclass.get_appUrl(config_file))  # cast to str
 
-        """
-        decode JSONString
-            method: com.decode.JwtUtils.mainDecode
-            input: (String) encoded JSONString
-            output: (String) JSONString
-        """
-        jwtclass = jpype.JClass("com.decode.JwtUtils")
-        log.info(jwtclass.mainDecode(jclass.readJSON(config_file)))
+            """
+            decode JSONString
+                method: com.decode.JwtUtils.mainDecode
+                input: (String) encoded JSONString
+                output: (String) JSONString
+            """
+            jwtclass = jpype.JClass("com.decode.JwtUtils")
+            log.info(jwtclass.mainDecode(jclass.readJSON(config_file)))
 
         self._dump_info(extract_folder, appUrl)
         # jpype.shutdownJVM()
@@ -70,7 +78,7 @@ class BufanApp(BaseModule):
 
 
 def main():
-    f = "./test_case/BufanApp/bufan1.apk"
+    f = "./test_case/BufanApp/世耀国际_bufan.apk"
     bufan = BufanApp(f, "android")
     if bufan.doSigCheck():
         logging.info("BufanApp signature Match")
