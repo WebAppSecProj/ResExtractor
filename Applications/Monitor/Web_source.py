10524e738a22b11d9ded43e7abaa013d21b7e1a9
FreshHillyer <472805416@qq.com>
2020-12-29 21:14:11 +0800

    Monitor

---
+++ Applications/Monitor/Web_source.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 19:17:30 2020
+
+@author: Hypo
+"""
+import os,sys,re,csv,time
+#from Url_base import HTML
+#from Monitor import WebMonitor
+        
+class Web_source():
+    def __init__(self, dirpath, appname):
+        if os.path.exists(dirpath):
+            self.dir = dirpath
+        else:
+            self.dir = None
+        self.appname = appname
+        self.url_list = None
+        self.format_list = None
+        self.notformat_list = None
+        self.HTTP_REGEX = 'https?://[a-zA-Z0-9\.\/_&=@$%?~#-]+'
+        self.IP_REGEX = '(((1[0-9][0-9]\.)|(2[0-4][0-9]\.)|(25[0-5]\.)|([1-9][0-9]\.)|([0-9]\.)){3}((1[0-9][0-9])|(2[0-4][0-9])|(25[0-5])|([1-9][0-9])|([0-9])))'
+        #非文本后缀
+        self.notfile = ["jpg","png","gif","bmp","webp","jepg","tgz"]
+        self.formats = ["jpg","png","gif","bmp","webp","jepg","tgz","txt","js","css"]
+        
+    @property
+    def allurl(self):
+        if self.url_list==None:
+            if self.dir:
+                self.url_list=[]
+                for root,dirs,files in os.walk(self.dir):
+                    for f in files:
+                        if any(f.endswith(i) for i in self.notfile):
+                            continue
+                        fp = os.path.join(root,f)
+                        try:
+                            with open(fp,'r',encoding='utf-8') as fs:
+                                self.url_list.extend(re.findall(self.HTTP_REGEX,fs.read()))
+                        except:
+                            continue
+                        self.url_list = list(set(self.url_list))
+                return self.url_list
+            return None
+        else:
+            return self.url_list
+        
+    def del_top(self,topfile):
+        if self.allurl == None:
+            return None
+        if topfile.endswith(".csv"):
+            try:               
+                with open(topfile,'r') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    column = [row['web'] for row in reader]
+                    for i in column:
+                        for j in self.allurl.copy():
+                            if i in j:
+                                self.allurl.remove(j)
+            except:
+                pass
+        if topfile.endswith(".txt"):
+            try:
+                with open(topfile,'r') as txt:
+                    column = txt.read().splitlines()
+                    for i in column:
+                        for j in self.allurl.copy():
+                            if i in j:
+                                self.allurl.remove(j)
+            except:
+                pass
+    
+    @property
+    def remote_url(self):
+        if self.allurl == None:
+            return None
+        self.format_list = []
+        self.notformat_list = []
+        for i in self.allurl.copy():
+            if any(i.endswith(t) for t in self.formats) :
+                self.format_list.append(i)
+            else:
+                self.notformat_list.append(i)
+        return self.format_list,self.notformat_list
+    
+    def dump(self, filepath, method="csv"):
+        if self.allurl == None:
+            return None
+        if self.notformat_list == None:
+            self.remote_url
+        if method=="csv":
+            if not os.path.exists(filepath):
+                with open(filepath,'w',newline='') as f:
+                    f_csv = csv.writer(f)
+                    f_csv.writerow(['appname','URL','folder'])
+                    for i in self.notformat_list:
+                        f_csv.writerow([self.appname,i,self.dir])
+            else:
+                with open(filepath,'a',newline='') as f:
+                    f_csv = csv.writer(f)
+                    for i in self.notformat_list:
+                        f_csv.writerow([self.appname,i,self.dir])
+        
+        
+                
+def test():
+    begin = time.time()
+    a=Web_source(r'G:\涉案APK\盘古\working_folder\working_folder\5ae6ee34631adfd2f35fa7e7994e9b91949f3d4f','test3')
+    a.del_top(r'G:\涉案APK\CN.csv')
+    a.del_top(r'C:\Users\hypo\Desktop\黑灰产paper\monitor\my_filter.txt')
+    b,c = a.remote_url
+    print(len(c))
+    #print(c)
+    #print(a.allurl)
+    print(len(a.allurl))
+    a.dump(r'C:\Users\hypo\Desktop\黑灰产paper\monitor\url.csv')
+    print(time.time()-begin)
+    
+if __name__== "__main__":
+    sys.exit(test())
---
+++ Applications/Monitor/demo.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 11:30:59 2020
+
+@author: hypo
+"""
+
+import os,sys,csv,time
+from Url_base import HTML
+from Web_source import Web_source
+from Monitor import WebMonitor
+
+class InputBase:
+    """
+    InputBase class.
+    InputBase is an iterator-based class, you first overwrite __iter__ for some context data (like current index of iterator).
+    Then, __next__ is called when Runner requests data.
+    """
+    def __iter__(self):
+        """
+        Default implementation is to return self.
+        """
+        return self
+
+    def __next__(self):
+        raise NotImplementedError(
+            "Subclass of InputBase should implement __next__ method.")
+        
+class DirInput(InputBase):
+    """
+    An example of InputBase implementation to list First level subdirectory for input folder.
+    Can also serve as a base class for Input that filters a directory. 
+    """
+    def __init__(self, folder: str):
+        self.folder = folder
+        self.iter = self.get_iter()
+
+    def get_iter(self):
+        """
+        The main entry is get_iter that returns an iterator of First level subdirectory.
+        """
+        return iter(
+            os.path.join(self.folder, a) for a in os.listdir(self.folder)
+            if os.path.isdir(os.path.join(self.folder, a)))
+
+    def __next__(self):
+        file_name = next(self.iter)
+        return file_name
+
+class Runner():
+    def __init__(self, filePath):
+        self.monitor = [filePath]
+        self.pwd = []
+        self.label = []
+        self.filter =[]
+        
+    def add_monitorfile(self,filePath):
+        self.monitor.append(filePath)
+    
+    def add_filter(self,filters):
+        self.filter.append(filters)
+        
+        
+    def add_websource(self,inp:InputBase,label):
+        self.pwd.append(inp)
+        self.label.append(label)
+        
+    def parser(self,dst_file):
+        for i in range(0,len(self.pwd)):
+            inp = self.pwd[i]
+            label = self.label[i]
+            for folder in inp:
+                print(folder)
+                a = Web_source(folder,label)
+                for i in self.filter:
+                    a.del_top(i)
+                a.dump(dst_file)
+
+        
+    def run(self,monitor = "all"):
+        if monitor == "all":
+            for monitor in self.monitor.copy():
+                if not os.path.exists(monitor):
+                    continue
+                try:
+                    with open(monitor,'r') as csvfile:
+                        new_dict=[]
+                        for row in csv.DictReader(csvfile):
+                            if WebMonitor(row['URL'],
+                                              row['folder'],
+                                              row['appname']):
+                                new_dict.append(row)
+                    with open(monitor,'w',newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow(['appname','URL','folder'])
+                        writer.writerows([i["appname"],i["URL"],i["folder"]]for i in new_dict)
+                    #检查完删除monitor列表
+                    self.monitor.remove(monitor)
+                except Exception as e:
+                    print("Monitor {} Error".format(e))
+        else:
+            if not os.path.exists(monitor):
+                return
+            try:
+                with open(monitor,'r') as csvfile:
+                    new_dict=[]
+                    for row in csv.DictReader(csvfile):
+                        if WebMonitor(row['URL'],
+                                          row['folder'],
+                                          row['appname']):
+                            new_dict.append(row)
+                with open(monitor,'w',newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(['appname','URL','folder'])
+                    writer.writerows([i["appname"],i["URL"],i["folder"]]for i in new_dict)
+                #检查完删除monitor列表
+                self.monitor.remove(monitor)
+            except Exception as e:
+                print("Monitor {} Error".format(e))
+    
+if __name__ == "__main__":
+    begin=time.time()
+    a = Runner(r'C:\Users\hypo\Desktop\黑灰产paper\monitor\url.csv')
+    #a.add_websource(DirInput(r'G:\涉案APK\盘古\working_folder\working_folder'),"test")
+    a.add_filter(r'G:\涉案APK\CN.csv')
+    a.add_filter(r'C:\Users\hypo\Desktop\黑灰产paper\monitor\my_filter.txt')
+    #a.parser(r'C:\Users\hypo\Desktop\黑灰产paper\monitor\url2.csv')
+    a.run()
+    #a.add_monitorfile(r'C:\Users\hypo\Desktop\黑灰产paper\monitor\url2.csv')
+    #a.run()
+    print(time.time()-begin)
